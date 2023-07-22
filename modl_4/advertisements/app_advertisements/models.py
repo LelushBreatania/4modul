82b1d8b66ad23c1c1fc3fc72300f3ed1d4352173
Александр <kernex245@yandex.ru>
2023-07-22 18:07:11 +0300

    Код с лекции №7

--- modl_4/advertisements/app_advertisements/models.py
+++ modl_4/advertisements/app_advertisements/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+class Advertisement(models.Model):
+
+    #Название тавара
+    # CharField - короткое текстовое поле
+    # 'Заголовок' - verbose_name - человекочитаемое название
+    title = models.CharField('Заголовок',max_length=128)
+
+    # Описание товара
+    description = models.TextField('описание')
+
+    # Цена
+    # Числовое поле с фиксированой точкой
+    price = models.DecimalField('цена', max_digits=10, decimal_places=2)
+
+    # Уместен ли торг
+    auction = models.BooleanField('торг', help_text='отметьте, если хотите торговаться')
+
+    # Дата публикации
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # Дата изменения/обнавления + что изменилось
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # Продавец
+
+    # Фото объявления
+
+    # Рейтинг
+
+    # В продаже/не в продаже - актуальность
+
+    
+
+    
+
+    
+
+
